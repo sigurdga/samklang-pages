71f47ff6246ae3bcb8702b7b7137e267499c617f
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-06-19 22:25:15 +0200

    tell find_packages where to search

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup, find_packages
 setup(
         name = 's7n-pages',
         version = "1a1",
-        packages = find_packages(),
+        packages = find_packages("s7n"),
         )
