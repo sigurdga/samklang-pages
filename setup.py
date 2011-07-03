341d4389556c8668e39e52ab119333c1fc892607
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-03 21:37:05 +0200

    add translations

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
         name = 's7n-pages',
         version = "1a1",
         packages = ['s7n.pages', 's7n.pages.migrations'],
-        package_data = {'s7n.pages': ['templates/pages/*.html']},
+        package_data = {'s7n.pages': ['templates/pages/*.html', 'locale/*/LC_MESSAGES/django.*o']},
         py_modules = ['s7n.pages.pagewidgets'],
         )
