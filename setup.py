e8e72ed5c0ebf7cf59880a25385d3ad538b5df14
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-20 13:13:22 +0200

    just try adding s7n module, should not make a difference

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
         name = 's7n-pages',
         version = "1a1",
-        packages = ['s7n.pages', 's7n.pages.migrations'],
+        packages = ['s7n', 's7n.pages', 's7n.pages.migrations'],
         package_data = {'s7n.pages': ['templates/pages/*.html', 'locale/*/LC_MESSAGES/django.*o']},
         py_modules = ['s7n.pages.pagewidgets'],
         )
