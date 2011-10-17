75cd6966cd6c16d0d572659c085f1693ce4da346
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-10-17 20:51:22 +0200

    Add CHANGES file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
         name='samklang-pages',
-        version="1a1",
+        version="0.1.0",
 	author='Sigurd Gartmann',
 	author_email='sigurdga-samklang@sigurdga.no',
 	url='http://github.com/sigurdga/samklang-pages',
