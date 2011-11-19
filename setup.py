eae87f8f1a33b639a1ff3e36f9bb87de6e75c04a
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-11-19 13:24:48 +0100

    Prepare for new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
         name='samklang-pages',
-        version="0.1.1",
+        version="0.1.2",
 	author='Sigurd Gartmann',
 	author_email='sigurdga-samklang@sigurdga.no',
 	url='http://github.com/sigurdga/samklang-pages',
