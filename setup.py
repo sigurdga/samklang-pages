958668abdd79bb23186e824df462c0d18db46e66
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-12 00:01:28 +0100

    Update version number and changes to 0.3.0

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from distutils.core import setup
 
 setup(
         name='samklang-pages',
-        version="0.2.0",
-	author='Sigurd Gartmann',
-	author_email='sigurdga-samklang@sigurdga.no',
-	url='http://github.com/sigurdga/samklang-pages',
-	description='Pages module for Samklang',
-	long_description=open('README.txt').read(),
-	license="AGPL",
+        version="0.3.0",
+        author='Sigurd Gartmann',
+        author_email='sigurdga-samklang@sigurdga.no',
+        url='http://github.com/sigurdga/samklang-pages',
+        description='Pages module for Samklang',
+        long_description=open('README.txt').read(),
+        license="AGPL",
         packages=['samklang_pages', 'samklang_pages.migrations'],
         package_data={'samklang_pages': ['templates/samklang_pages/*.html', 'locale/*/LC_MESSAGES/django.*o']},
         py_modules=['samklang_pages.pagewidgets'],
-	classifiers=[
-		"Development Status :: 3 - Alpha",
-		"License :: OSI Approved :: GNU Affero General Public License v3",
-		"Intended Audience :: Developers",
-		"Framework :: Django",
-		"Environment :: Web Environment",
-		"Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-		]
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: GNU Affero General Public License v3",
+            "Intended Audience :: Developers",
+            "Framework :: Django",
+            "Environment :: Web Environment",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            ]
         )
